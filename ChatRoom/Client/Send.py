7b511e5d2e8daec8f944b4cab732ff34460adc93
Mitch <67477144+Mitch0S@users.noreply.github.com>
2021-03-14 14:47:14 +0800

    Update Send.py
    Added a user join message

--- ChatRoom/Client/Send.py
+++ ChatRoom/Client/Send.py
@@ -1,12 +1,23 @@
 import requests
+import os
 
+os.system("clear")
 username = input("Please enter your username: ")
 address = input("Please enter the server address: ")
 port = input("Please enter the server port: ")
+
+credidentials = {
+    "Username": username
+}
+requests.post(("http://{}:{}/new_conn/").format(address, port), headers=credidentials)
+
 while True:
-    message = input("Message: ")
-    headers = {
-        "Username": username,
-        "Message": message
-    }
-    request.post(("http://{}:{}/post/").format(address, port)
+    try:
+        message = input("Message: ")
+        headers = {
+            "Username": username,
+            "Message": message
+        }
+        requests.post(("http://{}:{}/post/").format(address, port), headers=headers)
+    except:
+        print("An error has ocurred, retrying...")
