674b358d3cc444ec61be7b601002f7007700c4de
Mitch <67477144+Mitch0S@users.noreply.github.com>
2021-03-14 23:21:58 +0800

    Update Receive.py
    Added text file support for ease of use.

--- ChatRoom/Client/Receive.py
+++ ChatRoom/Client/Receive.py
@@ -3,12 +3,27 @@ import os
 import time
 
 os.system("clear")
-address = input("Please enter the server address: ")
-port = input("Please enter the server port: ")
+
+try:
+    file = open("Desktop/ChatRoom/INFORMATION.txt", "r+")
+    for line in file.readlines():
+        line = line.split(": ")
+        if line[0] == "ADDRESS":
+            address = line[1].strip()
+        if line[0] == "PORT":
+            port = line[1].strip()
+        if line[0] == "USERNAME":
+            username = line[1].strip()
+except:
+    print("Unable to find credidentials file.\n------------------------------------")
+    address = input("Please enter the server address: ")
+    port = input("Please enter the server port: ")
+
 while True:
     try:
         postTo = (("http://{}:{}/get/")).format(address, port)
         chat = requests.post(postTo)
+        oldChat = chat
         chats = chat.json()
         time.sleep(0.5)
         messages = chats.split("=+-=-+-")
