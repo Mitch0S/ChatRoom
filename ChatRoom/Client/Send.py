59deb869dd829b473617197e513e6d2146329ea9
Mitch <67477144+Mitch0S@users.noreply.github.com>
2021-03-14 23:23:05 +0800

    Update Send.py
    Added text file credentials support

--- ChatRoom/Client/Send.py
+++ ChatRoom/Client/Send.py
@@ -1,16 +1,29 @@
 import requests
 import os
-
 os.system("clear")
-username = input("Please enter your username: ")
-address = input("Please enter the server address: ")
-port = input("Please enter the server port: ")
+
+try:
+    file = open("Desktop/ChatRoom/INFORMATION.txt", "r+")
+    for line in file.readlines():
+        line = line.split(": ")
+        if line[0] == "ADDRESS":
+            address = line[1].strip()
+        if line[0] == "PORT":
+            port = line[1].strip()
+        if line[0] == "USERNAME":
+            username = line[1].strip()
+except:
+    print("Unable to find credidentials file.\n------------------------------------")
+    username = input("Please enter your username: ")
+    address = input("Please enter the server address: ")
+    port = input("Please enter the server port: ")
 
 credidentials = {
     "Username": username
 }
 requests.post(("http://{}:{}/new_conn/").format(address, port), headers=credidentials)
 
+print(("Hello, {}, welcome to Mitch's Chatroom!").format(username))
 while True:
     try:
         message = input("Message: ")
