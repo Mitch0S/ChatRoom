71b2a3e8a3b3d0e1081aebb249a87db955c46fc5
Mitch <67477144+Mitch0S@users.noreply.github.com>
2021-03-14 14:46:27 +0800

    Update Receive.py

--- ChatRoom/Client/Receive.py
+++ ChatRoom/Client/Receive.py
@@ -2,15 +2,18 @@ import requests
 import os
 import time
 
+os.system("clear")
 address = input("Please enter the server address: ")
 port = input("Please enter the server port: ")
 while True:
-    postTo = (("http://{}:{}/get/")).format(address, port)
-    requests.post(postTo)
-    chat = requests.post(postTo)
-    chats = chat.json()
-    time.sleep(1)
-    messages = chats.split("")
-    os.system("clear")
-    for i in messages:
-        print(i)
+    try:
+        postTo = (("http://{}:{}/get/")).format(address, port)
+        chat = requests.post(postTo)
+        chats = chat.json()
+        time.sleep(0.5)
+        messages = chats.split("=+-=-+-")
+        os.system("clear")
+        for i in messages:
+            print(i)
+    except:
+        print("An error has ocurred, continuing.")
