b5daccedb6deb09e36bc353dd1f3cd2ad311763e
Mitch <67477144+Mitch0S@users.noreply.github.com>
2021-03-15 16:14:23 +0800

    Update Server.py

--- ChatRoom/Server/Server.py
+++ ChatRoom/Server/Server.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/get/', methods=['POST'])
 def get_config():
-    chat = open("chat.txt", "r+").readlines()
+    chat = open("Desktop/ButlerChatRoomServer/chat.txt", "r+").readlines()
     chats = ""
     for line in chat:
         chats = chats+"=+-=-+-"+(line).strip()
@@ -16,12 +16,24 @@ def get_config():
 @app.route('/post/', methods=['POST'])
 def post_config():
     notAllowedCharacters = ["", " "]
+    notAllowedWords = []
+    try:
+        notAllowedWord = open("Desktop/ButlerChatRoomServer/notAllowedWords.txt").readlines()
+        for line in notAllowedWord:
+            notAllowedWords.append(line.strip())
+    except:
+        print("An error has ocurred while scanning the bad words file.")
     information = request.headers
     username = information["Username"]
     message = information["Message"]
-    chat = open("chat.txt", "a+")
+    message = message.strip()
+    chat = open("Desktop/ButlerChatRoomServer/chat.txt", "a+")
     if message in notAllowedCharacters:
         return jsonify("You entered an invalid message.")
+    messageSplit = message.split(" ")
+    for word in messageSplit:
+        if word.upper() in notAllowedWords:
+            return jsonify("Please refrain from swearing")
     else:
         chat.write(("[{}]: {}\n").format(username, message))
         return jsonify("Chat successfully sent.")
@@ -30,7 +42,7 @@ def post_config():
 def new_conn():
     information = request.headers
     username = information["Username"]
-    chat = open("chat.txt", "a+")
+    chat = open("Desktop/ButlerChatRoomServer/chat.txt", "a+")
     chat.write(("{} has joined the chat!\n").format(username))
     return jsonify("Successfully joined the chat room.")
 
